9c34568de5fec8201b66ff06477ef17ca9e096f7
Theo Martos <theo.martos@hotmail.fr>
2020-01-19 19:24:35 +0100

    Mutliples sorties

--- afficheur.py
+++ afficheur.py
@@ -2,11 +2,10 @@ import csv
 
 from perceptron import Perceptron
 
-
-def import_inputs():
+def import_inputs(path='inputs.csv'):
     inputs = []
 
-    with open('inputs.csv', newline='') as csv_file:
+    with open(path, newline='') as csv_file:
         csv_reader = csv.reader(csv_file)
         for index, row in enumerate(csv_reader):
             if index > 0:
@@ -14,28 +13,73 @@ def import_inputs():
 
     return inputs
 
-def write_inputs(inputs):
-    with open('inputs.csv', 'w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerow(['A', 'B', 'C', 'D', 'E', 'F', 'G'])
-        for input_row in inputs:
-            csv_writer.writerow(input_row)
+def test_perceptron_1(perceptron, inputs):
+    results = []
+
+    for possible_input in inputs.copy():
+        inp = possible_input[:]
+        inp.append(1)
+        results.append(int(round(perceptron.predict(inp, 0))))
+
+    print(results)
+
+def test_perceptron_4(perceptron, inputs):
+    results = []
+
+    for inp in inputs:
+        res = 0
+        for i in range(4):
+            sg = inp[:]
+            sg.append(1)
+            res = res << 1
+            res += int(round(perceptron.predict(sg, i)))
+        results.append(res)
+
+    print(results)
 
 def main():
-    outputs = range(1, 10)
     inputs = import_inputs()
+    
+    outputs_4 = [
+        [0, 0, 0, 1],
+        [0, 0, 1, 0],
+        [0, 0, 1, 1],
+        [0, 1, 0, 0],
+        [0, 1, 0, 1],
+        [0, 1, 1, 0],
+        [0, 1, 1, 1],
+        [1, 0, 0, 0],
+        [1, 0, 0, 1]
+    ]
 
-    perceptron = Perceptron(0.01, 0.005, 1)
-    perceptron.train(inputs, outputs)
-    # perceptron.load_weight()
+    outputs_1 = []
+    for i in range(1, 10):
+        outputs_1.append([i])
+    # outputs_1 = [
+    #     [1],
+    #     [2],
+    #     [3],
+    #     [4],
+    #     [6],
+    #     [5],
+    #     [7],
+    #     [8],
+    #     [9],
+    # ]
 
-    for possible_input in inputs:
-        print(round(perceptron.predict(possible_input)))
+    perceptron_1 = Perceptron(7, 1, 0.01, 0.001, prediction_type=1)
+    perceptron_4 = Perceptron(7, 4, 0.01, 0.002, prediction_type=1)
 
-    perceptron.show_error_graph()
+    perceptron_1.train(inputs, outputs_1)
+    # perceptron_1.train(import_inputs('inputs65.csv'), outputs_1)
+    perceptron_4.train(inputs, outputs_4)
 
-    perceptron.save_weight()
+    test_perceptron_1(perceptron_1, import_inputs())
+    # test_perceptron_4(perceptron_4, inputs)
 
+    perceptron_4.show_error_graph()
+
+    # perceptron.save_weight()
 
 
 if __name__ == "__main__":
--- perceptron.py
+++ perceptron.py
@@ -1,61 +1,68 @@
+import numpy as np
 import csv
-
+import math
 import matplotlib.pyplot as plt
-
 
 class Perceptron:
 
-    def __init__(self, learning_rate, delta, prediction_type = 0):
-        self._input_count = 7
-        self._epoch_count = 0
+    def __init__(self, nb_inputs, nb_outputs, learning_rate, delta, prediction_type = 0, biais = 1):
+        self._inputs_count = nb_inputs + 1
+        self._outputs_count = nb_outputs
         self._learning_rate = learning_rate
         self._prediction_type = prediction_type
         self._delta = delta
-        self._weigths = [0 for _ in range(7)]
-        self._biais = 1
-        self._biais_weigth = 0
+        self._weights = np.zeros((self._outputs_count, self._inputs_count))
         self._error_values = []
+        self._biais = biais
 
-    def predict(self, inputs):
-        computed_res = 0
-        for i, input_val in enumerate(inputs):
-            computed_res += input_val * self._weigths[i]
-
-        computed_res += self._biais_weigth
+    def predict(self, input_set, output_nb):
+        res = np.dot(input_set, self._weights[output_nb])
 
         if self._prediction_type == 0:
-            return 0 if computed_res <= 0 else 1
+            return 0 if res <= 0 else 1
         elif self._prediction_type == 1:
-            return 0 if computed_res <= 0 else computed_res
+            return 0 if res <= 0 else res
 
-    def train(self, inputs, expected_outputs):
+    def train(self, all_inputs, expected_outputs):
 
         keep_training = True
-        last_error_rate_average = 1000
+        last_error_rate_average = math.inf
 
+        epoch_count = 0
         while keep_training:
             epoch_error_value = 0
-            for i, segments_configuration in enumerate(inputs):
+            
+            for output_nb in range(self._outputs_count):
+                epoch_error_output_value = 0
+                for i, segments_configuration in enumerate(all_inputs):
+                    sg = segments_configuration[:]
+                    sg.append(self._biais)
+                    epoch_error_output_value += self.update_weights(sg, 
+                     expected_outputs[i], output_nb)
                 
-                prediction = self.predict(segments_configuration)
-
-                for j, weight in enumerate(self._weigths):
-                    new_weight = weight + self._learning_rate * \
-                        (expected_outputs[i] - prediction) * segments_configuration[j]
-                    self._weigths[j] = new_weight
-
-                self._biais_weigth = self._biais_weigth + \
-                    self._learning_rate * \
-                    (expected_outputs[i] - prediction) * 1
-
-                epoch_error_value += 0.5 * \
-                    ((prediction - expected_outputs[i]) ** 2)
+            epoch_error_value = 0.5 * epoch_error_output_value
             self._error_values.append(epoch_error_value)
 
-            new_error_rate_average = sum(self._error_values) / len(self._error_values)
+            new_error_rate_average = sum(self._error_values) / \
+                len(self._error_values)
             if abs(new_error_rate_average - last_error_rate_average) < self._delta:
                 keep_training = False
             last_error_rate_average = new_error_rate_average
+            epoch_count += 1
+            
+        print("{} epochs".format(epoch_count))
+
+    def update_weights(self, input_set, expected_output, output_nb):
+        weights = self._weights[output_nb]
+        prediction = self.predict(input_set, output_nb)
+
+        for i, weight in enumerate(weights):
+            new_weight = weights[i] + self._learning_rate * (expected_output[output_nb] - prediction) * input_set[i]
+            weights[i] = new_weight
+        
+        epoch_error_value = (prediction - expected_output[output_nb]) ** 2
+
+        return epoch_error_value
 
     def show_error_graph(self):
         plt.plot(self._error_values)
@@ -65,19 +72,24 @@ class Perceptron:
         filename = 'perceptron.csv'
         with open(filename, 'w', newline='') as csvfile:
             csv_writer = csv.writer(csvfile)
-            header_line = ['w1', 'w2', 'w3', 'w4', 'w5', 'w6', 'w7', 'w biais']
+            header_line = ['output', 'w1', 'w2', 'w3', 'w4', 'w5', 'w6', 'w7', 'w biais']
             csv_writer.writerow(header_line)
-            csv_writer.writerow(self._weigths + [self._biais_weigth])
+            for k, output_weights in enumerate(self._weights):
+                output_weights = np.append([k], output_weights)
+                csv_writer.writerow(output_weights)
 
     def load_weight(self):
         filename = 'perceptron.csv'
         with open(filename, 'r', newline='') as csvfile:
             csv_reader = csv.reader(csvfile, delimiter=',')
-            for line in csv_reader:
-                weights = line
-            for i in range(len(weights) - 1):
-                self._weigths[i] = float(weights[i])
-            self._biais_weigth = float(weights[-1])
 
-    def __str__(self):
-        return "Perceptron [weights = {}, biais weight = {}]".format(self._weigths, self._biais_weigth)
+            outputs_weights = []
+            for l, line in enumerate(csv_reader):
+                if l != 0:
+                    outputs_weights.append(line)
+
+            weights = []
+            for output_weights in outputs_weights:
+                weights.append(list(map(lambda x: float(x), output_weights[1:])))
+
+            self._weights = weights.copy()
